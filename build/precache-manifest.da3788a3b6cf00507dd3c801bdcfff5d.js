729367cd8b06fd383b9f2ec5fd3b952de1336089
rajatrai31 <rajat.intern@itday.in>
2022-10-25 23:26:31 +0530

    commit 2

---
+++ build/precache-manifest.da3788a3b6cf00507dd3c801bdcfff5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e51c15c310d63d3927d0b0e7962dbfb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32d859f882417df5f8ae",
+    "url": "/static/css/2.572b526e.chunk.css"
+  },
+  {
+    "revision": "343bfc20cfd9a127cb74",
+    "url": "/static/css/main.9b1d91b3.chunk.css"
+  },
+  {
+    "revision": "32d859f882417df5f8ae",
+    "url": "/static/js/2.5464a5af.chunk.js"
+  },
+  {
+    "revision": "59cff7655571963986f6ca7e065ed12b",
+    "url": "/static/js/2.5464a5af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "343bfc20cfd9a127cb74",
+    "url": "/static/js/main.b58af87f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "3ae3c1326364ef1d2381cf12d6b4109e",
+    "url": "/static/media/team.3ae3c132.svg"
+  }
+]);
